aacfa1dbb19e9b66d0ec7359d6309a1cec518d83
thedarkzeno <multiolx@gmail.com>
2020-01-16 18:18:30 -0300

    backend

---
+++ backend/src/controllers/dev.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const Dev = require("../models/Dev.js");
+const parseStringAsArray = require("../utils/parseStringAsArray");
+
+module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  },
+
+  async store(request, response) {
+    const { github_username, techs, longitude, latitude } = request.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      const githubResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
+
+      const { name = login, avatar_url, bio } = githubResponse.data;
+
+      const techsArray = parseStringAsArray(techs);
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location: {
+          type: "Point",
+          coordinates: [longitude, latitude]
+        }
+      });
+    }
+
+    response.json(dev);
+  }
+};
